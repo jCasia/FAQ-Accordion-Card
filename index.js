6ecc5a222678505b0d12498182a8b88609e4af58
jCasia <casjay70@gmail.com>
2023-02-09 01:47:21 +0000

    fixed the layout and background, added JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const accordionContent = document.querySelectorAll(".faq__accordion");
+
+accordionContent.forEach((el) => {
+  const header = el.querySelector(".faq__header");
+  header.addEventListener("click", () => {
+    el.classList.toggle("open");
+
+    const headerHeading = el.querySelector(".faq__accordion-heading");
+    const answer = el.querySelector(".faq__accordion-text");
+    const iconSwitch = el.querySelector(".icon");
+
+    if (el.classList.contains("open")) {
+      answer.style.height = `${answer.scrollHeight}px`;
+
+      headerHeading.style.fontWeight = "700";
+      headerHeading.style.color = "--color-very-dark-saturated-blue";
+      iconSwitch.classList.replace("fa-chevron-down", "fa-chevron-up");
+    } else {
+      answer.style.height = 0;
+      headerHeading.style = "";
+      iconSwitch.classList.replace("fa-chevron-up", "fa-chevron-down");
+    }
+  });
+});
