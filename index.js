e87d50fcf6d991b18ed1d8b670142b7531d13f6b
jCasia <casjay70@gmail.com>
2023-02-09 23:08:39 +0000

    Updated JavaScript

--- index.js
+++ index.js
@@ -2,25 +2,45 @@
 
 const accordionContent = document.querySelectorAll(".faq__accordion");
 
-accordionContent.forEach((el) => {
+accordionContent.forEach((el, index) => {
   const header = el.querySelector(".faq__header");
+  const headerHeading = el.querySelector(".faq__accordion-heading");
+  const accordionAnswer = el.querySelector(".faq__accordion-text");
+  const iconSwitch = el.querySelector(".icon");
+
   header.addEventListener("click", () => {
     el.classList.toggle("open");
 
-    const headerHeading = el.querySelector(".faq__accordion-heading");
-    const answer = el.querySelector(".faq__accordion-text");
-    const iconSwitch = el.querySelector(".icon");
-
     if (el.classList.contains("open")) {
-      answer.style.height = `${answer.scrollHeight}px`;
-
+      accordionAnswer.style.height = `${accordionAnswer.scrollHeight}px`;
       headerHeading.style.fontWeight = "700";
       headerHeading.style.color = "--color-very-dark-saturated-blue";
       iconSwitch.classList.replace("fa-chevron-down", "fa-chevron-up");
     } else {
-      answer.style.height = 0;
+      accordionAnswer.style.height = 0;
       headerHeading.style = "";
       iconSwitch.classList.replace("fa-chevron-up", "fa-chevron-down");
     }
+
+    callbackCloseRest(index);
   });
 });
+
+//callback function using the index (eg. the index of the accordion that was clicked),
+// loop through accordions and if any of them does not equal to the index of the accordion that was clicked,
+// remove class "open" and update the styles.
+function callbackCloseRest(index) {
+  accordionContent.forEach((accordion, index2) => {
+    if (index !== index2) {
+      accordion.classList.remove("open");
+
+      const accordionAnswer2 = accordion.querySelector(".faq__accordion-text");
+      const headerHeading2 = accordion.querySelector(".faq__accordion-heading");
+      const iconSwitch2 = accordion.querySelector(".icon");
+
+      headerHeading2.style = "";
+      accordionAnswer2.style.height = 0;
+      iconSwitch2.classList.replace("fa-chevron-up", "fa-chevron-down");
+    }
+  });
+}
